c62124cf08b91774416e07d6edf30f16e35446cc
TajVeerThiara <tajveer.thiara@gmail.com>
2017-01-29 14:39:52 -0800

    Add files via upload

---
+++ Hello(2).asm
@@ -0,0 +1,172 @@
+$MODLP52
+org 0000H
+   ljmp MainProgram
+
+CLK  EQU 22118400
+BAUD equ 115200
+T1LOAD equ (0x100-(CLK/(16*BAUD)))
+CE_ADC EQU P2.0
+MY_MOSI EQU P2.1
+MY_MISO EQU P2.2
+MY_SCLK EQU P2.3
+CSEG
+
+dseg at 0x30
+Result:     ds  2
+x:   ds 4
+y:   ds 4
+bcd: ds 5
+
+BSEG
+mf: dbit 1
+
+$NOLIST
+$include(math32.inc)
+$LIST
+
+CSEG
+INIT_SPI:
+ setb MY_MISO ; Make MISO an input pin
+ clr MY_SCLK ; For mode (0,0) SCLK is zero
+ ret
+
+DO_SPI_G:
+ push acc
+ mov R1, #0 ; Received byte stored in R1
+ mov R2, #8 ; Loop counter (8-bits)
+DO_SPI_G_LOOP:
+ mov a, R0 ; Byte to write is in R0
+ rlc a ; Carry flag has bit to write
+ mov R0, a
+ mov MY_MOSI, c
+ setb MY_SCLK ; Transmit
+ mov c, MY_MISO ; Read received bit
+ mov a, R1 ; Save received bit in R1
+ rlc a
+ mov R1, a
+ clr MY_SCLK
+ djnz R2, DO_SPI_G_LOOP
+ pop acc
+ ret
+
+WaitHalfSec:
+    mov R2, #89
+L3: mov R1, #250
+L2: mov R0, #166
+L1: djnz R0, L1 ; 3 cycles->3*45.21123ns*166=22.51519us
+    djnz R1, L2 ; 22.51519us*250=5.629ms
+    djnz R2, L3 ; 5.629ms*89=0.5s (approximately)
+    ret
+
+Send_BCD mac
+    push ar0
+    mov r0, %0
+    lcall ?Send_BCD
+    pop ar0
+endmac
+
+?Send_BCD:
+    push acc
+    ; Write most significant digit
+    mov a, r0
+    swap a
+    anl a, #0fh
+    orl a, #30h
+    lcall putchar
+    ; write least significant digit
+    mov a, r0
+    anl a, #0fh
+    orl a, #30h
+    lcall putchar
+    pop acc
+    ret
+    
+; Configure the serial port and baud rate using timer 1
+InitSerialPort:
+    ; Since the reset button bounces, we need to wait a bit before
+    ; sending messages, or risk displaying gibberish!
+    mov R1, #222
+    mov R0, #166
+    djnz R0, $   ; 3 cycles->3*45.21123ns*166=22.51519us
+    djnz R1, $-4 ; 22.51519us*222=4.998ms
+    ; Now we can safely proceed with the configuration
+	clr	TR1
+	anl	TMOD, #0x0f
+	orl	TMOD, #0x20
+	orl	PCON,#0x80
+	mov	TH1,#T1LOAD
+	mov	TL1,#T1LOAD
+	setb TR1
+	mov	SCON,#0x52
+    ret
+
+; Send a character using the serial port
+putchar:
+    jnb TI, putchar
+    clr TI
+    mov SBUF, a
+    ret
+
+; Send a constant-zero-terminated string using the serial port
+SendString:
+    clr A
+    movc A, @A+DPTR
+    jz SendStringDone
+    lcall putchar
+    inc DPTR
+    sjmp SendString
+SendStringDone:
+    ret
+ 
+Empty:
+	DB	'\r\n',0
+MainProgram:
+    mov SP, #7FH ; Set the stack pointer to the begining of idata
+    mov PMOD, #0 ; Configure all ports in bidirectional mode
+        
+    lcall InitSerialPort
+  
+    lcall INIT_SPI
+forever:    
+    clr CE_ADC
+    mov R0, #00000001B
+    lcall DO_SPI_G
+    
+    mov R0, #10000000B ; Single ended, read channel 0
+    lcall DO_SPI_G
+    mov a, R1 ; R1 contains bits 8 and 9
+    anl a, #00000011B ; We need only the two least significant bits
+    mov Result+1, a ; Save result high.
+    
+    mov R0, #55H ; It doesn't matter what we transmit...
+    lcall DO_SPI_G
+    mov Result, R1 ; R1 contains bits 0 to 7. Save result low.
+    setb CE_ADC
+    
+    lcall WaitHalfSec
+    
+    mov x+3, #0
+    mov x+2, #0
+    mov x+1, Result+1
+    mov x+0, Result
+    load_y (497)
+    lcall mul32
+    load_y (1023)
+    lcall div32
+    
+    ; for displaying temperature ( C)
+    load_y(273)
+    lcall sub32
+    
+    lcall hex2bcd
+    
+    Send_BCD(bcd+1)
+    ;mov a, #'.'
+    ;lcall putchar
+    Send_BCD(bcd)
+    
+    mov DPTR, #Empty
+    lcall SendString
+    sjmp forever
+    
+END
